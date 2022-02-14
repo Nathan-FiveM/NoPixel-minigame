39f2c949e730f968979c028e3c27ad5f86fa561d
Arcadia Roleplay <arcadiaroleplaygtav@gmail.com>
2022-02-14 04:56:29 +0000

    Updated Hack Game to include variables
    Updated hack game to include the variables for time and blocks etc...

--- fivem-script/hacking/client/main.lua
+++ fivem-script/hacking/client/main.lua
@@ -1,35 +1,37 @@
-local successCb
-local failCb
-local resultReceived = false
+local IsHacking = false
+
+AddEventHandler('open:minigame', function(callback)
+    Callbackk = callback
+    openHack()
+end)
+
+function OpenHackingGame(puzzleDuration, puzzleLength, puzzleAmount, callback)
+    Callbackk = callback
+    openHack(puzzleDuration, puzzleLength, puzzleAmount)
+end
 
 RegisterNUICallback('callback', function(data, cb)
-    SetNuiFocus(false, false)
-    resultReceived = true
-    if data.success then
-        successCb()
-    else
-        failCb()
-    end
+    closeHack()
+    Callbackk(data.success)
     cb('ok')
 end)
 
-RegisterCommand('hack', function(source, args)
-    -- Please check the parameters below for exports
-    exports["hacking"]:hacking(
-    function() -- success
-        print("success")
-    end,
-    function() -- failure
-        print("failure")
-    end)
-end)
-
-exports('hacking', function(success, fail)
-    resultReceived = false
-    successCb = success
-    failCb = fail
+function openHack(puzzleDuration, puzzleLength, puzzleAmount)
     SetNuiFocus(true, true)
     SendNUIMessage({
-        action = "open"
+        action = "open",
+        duration = puzzleDuration,
+        length = puzzleLength,
+        amount = puzzleAmount
     })
-end)
+    IsHacking = true
+end
+
+function closeHack()
+    SetNuiFocus(false, false)
+    IsHacking = false
+end
+
+function GetHackingStatus()
+    return IsHacking
+end
